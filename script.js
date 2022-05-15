fc2c4db21038004f20e8a349d4ab833145018bc0
Anaconda04777 <72622363+Anaconda04777@users.noreply.github.com>
2022-05-15 20:00:08 +0200

    dd

--- script.js
+++ script.js
@@ -10,27 +10,43 @@ window.onload = function() {
 }
 
 class Card {
-    constructor(name, image, manaCost, power, toughness, text) {
+    constructor(name, image, manaCost, power, toughness, text, color, typeLine) {
         this.name = name
         this.image = image
         this.manaCost = manaCost
-        this.power = power
+        this.power = power + "/"
         this.toughness = toughness
         this.text = text
+        this.color = color[0]
+        this.typeLine = typeLine
+
+        if (color.length > 1) {
+            this.color = "Gold"
+        }
+
+        if (power === undefined || toughness === undefined) {
+            this.power = ""
+            this.toughness = ""
+        }
+
     }
 
     generazione() {
-        let str = `<div class="card">
-                        <div>
+        let str = `<div class="card ${this.color}">
+                        <div class="headerCard">
                             <h1>${this.name}</h1>
                             <p>${this.manaCost}</p>
                         </div>
-                        
-                        <img scr="${this.image}" class="imageCard">
-                        <p class="text">${this.text}</p>
+                        <img src="${this.image}" class="imageCard" load="lazy">
+                        <div class="mainCard">
+                            
+                            <h3>${this.typeLine}</h3>
+                            <p class="text">${this.text}</p>
+                        </div>
+
                         <div class="stats">
-                            <p>${this.power}</p>
-                            <p>${this.toughness}</p>
+                            <p>${this.power} </p>
+                            <p> ${this.toughness}</p>
                         </div>
                     </div>
                     `
@@ -42,7 +58,6 @@ function creazioneGrid() {
     //cercare carte nel set
     //let array = request("https://api.scryfall.com/sets/emn")
     for (let i = 1; i < numCarte; i++) {
-        console.log("dio")
         requestCarte(i)
         
     }
@@ -51,7 +66,8 @@ function creazioneGrid() {
 
 
 function creazioneOggCarta(info) {
-    let ogg = new Card(info.name, info.image_uris["art_crop"],info.mana_cost, info.power, info.toughness, info.oracle_text);
+    let ogg = new Card(info.name, info.image_uris["art_crop"],info.mana_cost, info.power, info.toughness, 
+                        info.oracle_text, info.colors, info.type_line);
     listaOggetti.push(ogg)
 
     document.querySelector("main").innerHTML += ogg.generazione()
